a2d8fafc3a32fcdd6ffc913ccf46f723d1376290
Riyan Dhiman <riyandhiman14@gmail.com>
2024-01-27 23:48:06 +0530

    some logo fixes and added splash screen

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as SplashScreen from 'expo-splash-screen';
 export default function App() {
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 1500);
   return (
     // <View style={styles.container}>
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
-    <WebView
+    <WebView style={styles.container}
       source={{ uri: 'https://doc-analyzer.vercel.app'}}
       // style={{ marginTop: '10%'}}
       sharedCookiesEnabled={true}
