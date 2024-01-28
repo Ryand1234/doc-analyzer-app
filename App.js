005f42aca541f0743595a2ac35cd30b7eebcc007
Riyan Dhiman <riyandhiman14@gmail.com>
2024-01-28 08:04:23 +0530

    Added Animation on splash screen

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
-import * as SplashScreen from 'expo-splash-screen';
-export default function App() {
-
-SplashScreen.preventAutoHideAsync();
-setTimeout(SplashScreen.hideAsync, 1500);
-  return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <WebView style={styles.container}
-      source={{ uri: 'https://doc-analyzer.vercel.app'}}
-      // style={{ marginTop: '10%'}}
-      sharedCookiesEnabled={true}
-      thiredPartyCookieEnabled={true}
-    />
-  );
-}
+import Constants from "expo-constants";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  Animated,
+  StyleSheet,
+  View,
+} from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -28,3 +16,103 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// Instruct SplashScreen not to hide yet, we want to do this manually
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
+
+export default function App() {
+  return (
+    <AnimatedAppLoader image={Constants.expoConfig.splash.image }>
+      <WebView style={styles.container}
+      source={{ uri: 'https://doc-analyzer.vercel.app'}}
+      // style={{ marginTop: '10%'}}
+      sharedCookiesEnabled={true}
+      thiredPartyCookieEnabled={true}
+    />
+    </AnimatedAppLoader>
+  );
+}
+
+function AnimatedAppLoader({ children, image }) {
+  const [isSplashReady, setSplashReady] = useState(true);
+
+  // useEffect(() => {
+  //   async function prepare() {
+  //     await Asset.fromURI(image.uri).downloadAsync();
+  //     setSplashReady(true);
+  //   }
+
+  //   prepare();
+  // }, [image]);
+
+  if (!isSplashReady) {
+    return null;
+  }
+
+  return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>;
+}
+
+function AnimatedSplashScreen({ children, image }) {
+  const animation = useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = useCallback(async () => {
+    try {
+      await SplashScreen.hideAsync();
+      // Load stuff
+      await Promise.all([]);
+    } catch (e) {
+      // handle errors
+    } finally {
+      setAppReady(true);
+    }
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.expoConfig.splash.backgroundColor,
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: Constants.expoConfig.splash.resizeMode || "contain",
+              transform: [
+                {
+                  scale: animation,
+                },
+              ],
+            }}
+            src={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
